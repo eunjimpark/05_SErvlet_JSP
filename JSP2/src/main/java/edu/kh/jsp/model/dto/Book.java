4b66069d46f024bcfdfbc5ea899e1fa35ef687a7
ParkTina <tina8140@hanmail.net>
2024-02-22 16:44:24 +0900

    240222

---
+++ JSP2/src/main/java/edu/kh/jsp/model/dto/Book.java
@@ -0,0 +1,51 @@
+package edu.kh.jsp.model.dto;
+
+public class Book {
+	
+	private String title;
+	private String writer;
+	private int price;
+	
+	//기본생성자
+	public Book() {}
+	
+	//매개변수생성자
+	public Book(String title, String writer, int price) {
+		super();
+		this.title = title;
+		this.writer = writer;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", writer=" + writer + ", price=" + price + "]";
+	}
+	
+
+
+}
